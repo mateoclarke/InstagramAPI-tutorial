84c5e5347602da885218b2e41585ca69a5179859
mateoclarke <code@mateoclarke.com>
2013-12-18 00:34:47 -0600

    branch to skip past css and html copy paste job

--- instaclown.rb
+++ instaclown.rb
@@ -6,29 +6,3 @@ get '/' do
   @photos = recent_photos
   erb :all_photos 
 end  
-
-get '/:resolution/:hashtag' do
-  @title = 'Photos'  
-  @photos = recent_photos(params[:hashtag],params[:resolution])
-  erb :all_photos
-end
-
-post '/search' do
-  @title = params["hashtag"].capitalize
-  @photos = recent_photos(params["hashtag"],params["resolution"])
-  erb :all_photos
-end
-
-def recent_photos (hashtag = "clown", resolution = "thumbnail")
-  @photos = []
-  instagram_response = "https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=[CLIENT_ID]"
-  response = Unirest.get(instagram_response)
-  photos_array = response.body["data"]
-
-  photos_array.each do |photo|
-    image = photo["images"][resolution]["url"]
-    @photos << image
-  end
-
-  @photos
-end 
